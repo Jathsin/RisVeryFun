a54c7b1aebaa239db5a31a61f1a952c3d93a340f
Jathsin <jathsin@Juans-MacBook-Air.local>
2025-11-05 09:44:19 +0100

    added formulas

--- Cheatsheet.R
+++ Cheatsheet.R
@@ -2,8 +2,7 @@
 n <- 10
 x <- 1:n
 
-# DISTRIBUTIONS
-
+# DISTRIBUTION + CUMULATIVE
 
 #Normal
 
@@ -12,4 +11,12 @@ x <- 1:n
 #Bernoulli
 
 #Binomial
+# Definition
 P <- dbinom(x,n, 0.5) # x can be either a vector or a number
+
+# Cumulative
+# probability of obtaining 4 our fewer heads, these are equivalent
+p_four_fewer_heads <- sum(dbinom(c(1,2,3,4), n, p_heads))
+p_four_fewer_heads_2 <- pbinom(4,10,0.7,lower.tail=TRUE, log.p=FALSE) #prefer this one
+
+
