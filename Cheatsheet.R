9656c05c988053d0e1fab67d1049a6a6e10fba48
Jathsin <jathsin@Juans-MacBook-Air.local>
2025-11-19 10:12:20 +0100

    ee

--- Cheatsheet.R
+++ Cheatsheet.R
@@ -71,5 +71,13 @@ p <- p_poison_greater(450, 400)
 
 
 #CHAPTER 6: Statistical Inference 
-
+variance <- function(x, y, estimator) {
+  mean <- sum(x*y)/sum(y)
+  num <- sum((x-mean)**2)
+  N <- sum(y)
+  if (estimator) {
+    N <- N-1
+  }
+  num/N
+}
   
